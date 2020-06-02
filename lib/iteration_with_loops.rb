74a5e0a4486aadc8e42c4d98ad889e61209015d2
Liz Love <love.elizabeth1989@gmail.com>
2020-06-02 20:42:31 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,15 @@
 def join_nested_strings(src)
+  row_index = 0
+  count = 0
+  while row_index < src.count do
+    element_index = 0
+    while element_index < src[row_index].count do
+      if src[row_index][element_index].include?(" ")
+        
+    
+    end
+  end
+  
 end
 
 src[row_index][element_index].is_a? Integer
