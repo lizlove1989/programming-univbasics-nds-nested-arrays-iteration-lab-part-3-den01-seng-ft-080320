66b9ad95c100d50e2424ee659b5f42a3c90555cd
Liz Love <love.elizabeth1989@gmail.com>
2020-06-02 20:46:40 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -10,7 +10,7 @@ def join_nested_strings(src)
         strings << src[row_index][element_index]
         element_index += 1
       end
-    element_row += 1
+    element_index += 1
     end
   row_index += 1
   end
