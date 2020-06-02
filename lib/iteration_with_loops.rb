19b76f5895308aebd82b36d51f22bd2feb1df505
Liz Love <love.elizabeth1989@gmail.com>
2020-06-02 21:15:40 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ def join_nested_strings(src)
   while row_index < src.count do
     element_index = 0
     while element_index < src[row_index].count do
-      if src[row_index][element_index].delete_if{ |a| a.is_a? Integer}
+      if src[row_index].delete_if{ |a| a.is_a? Integer}
         element_index += 1
       end
     element_index += 1
